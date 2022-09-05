770fe6a56fa4388c8039dffa6efec7e63f4e9112
kgisaackg <kg21isaackg21@gmail.com>
2022-09-05 16:43:09 +0200

    Started working on the myproperty.js script

--- gumtree.js
+++ gumtree.js
@@ -1,27 +1,85 @@
-// in a new folder be sure to run "npm init -y" and "npm install puppeteer"
-const puppeteer = require("puppeteer")
-const fs = require("fs/promises")
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const { start } = require('repl');
+const writeStream = fs.createWriteStream('properties by preferental.json')
 
-async function start() {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  await page.goto("https://www.property24.com/to-rent/agency/preferental-platform/preferental-platform/233159")
-//   await page.goto("https://learnwebcode.github.io/practice-requests/")
+//Detemines how many times . appears in a string 
+function count(str, find) {
+  return (str.split(find)).length - 1;
+}
+// Headers
+// writeStream.write('[ \n');
+// let link = 'https://www.privateproperty.co.za/estate-agency/preferental/rental-listings/10490'
+let link = 'https://www.gumtree.co.za/u-seller-listings/preferental-platform/v1u114570700p'
+let individual_links = []
+let property_link;
+let default_link = 'https://www.gumtree.co.za'
+// let prefId;
+//  for (let x = 0; x < 999; x++) {
 
-//   const names = await page.evaluate(() => {
-//     return Array.from(document.querySelectorAll(".info strong")).map(x => x.textContent)
-//   })
+//  }
 
-// const price = await page.evaluate(() => {
-//     return Array.from(document.querySelectorAll(".p24_price")).map(x => x.textContent)
-//   }) 
-let price = await page.evaluate(() => {
-    return Array.from(document.querySelectorAll(".p24_price")).map(x => x.textContent)
-  })
+function scrape() {
 
-  console.log("price", price);
+  for (let i = 1; i < 20; i++) {
+    request(`${link}${i}`, (err, response, html) => {
+      
+      if (!err) {
+        const $ = cheerio.load(html)
+  
+        //Get other files
+        $('.related-ad-title').each((index, elem) => {
+          var url = $(elem).attr('href')
+          individual_links.push(url)
+  
+  
+          // console.log("individual_links:", individual_links);
+          property_link = default_link + individual_links[i]
+          request(property_link, (err, response, html) => {
+            if (!err) {
+              const p = cheerio.load(html)
+  
+              //Get other files
+              p('.description-content').each((index, el) => {
+                var prefId = p(el).children('span').children('.phoneclick-increment ').children('a').children('href')
+                // console.log("Im calling u");
+                console.log("PrefID: ", prefId);
+              });
 
-  await browser.close()
+            }
+          });
+        });
+        // $('.pagination').each((index, elem) => {
+        //     const pages = $(elem).find('.pageNumber').text()
+        //     max = pages.substring(pages.indexOf('7') + 1, pages.indexOf('Next'))
+        //     mogs = parseInt(max)
+        //     console.log("from pagination: ", mogs)
+  
+        // });
+      }
+    });
+  }
 }
 
-start()
+scrape();
+
+// // in a new folder be sure to run "npm init -y" and "npm install puppeteer"
+// const puppeteer = require("puppeteer")
+// const fs = require("fs/promises")
+
+// async function start() {
+//   const browser = await puppeteer.launch()
+//   const page = await browser.newPage()
+//   await page.goto("https://www.gumtree.co.za/u-seller-listings/preferental-platform/v1u114570700p1")
+
+// let price = await page.evaluate(() => {
+//     return Array.from(document.getElementsByClassName('.title title-mult-lines')).map(x => x.textContent)
+//   })
+
+//   console.log("price", price);
+
+//   await browser.close()
+// }
+
+// start()
